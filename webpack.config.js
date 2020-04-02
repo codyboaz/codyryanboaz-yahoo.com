9b3dd0f6d7bcd769cb59234a93936ec15308a9cf
Cody Boaz <codyryanboaz@yahoo.com>
2020-04-02 16:25:47 -0700

    adding environment variables

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,23 @@
 const path = require('path')
-const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const dotenv = require('dotenv');
 
-module.exports = () => {
-  // call dotenv and it will return an Object with a parsed key 
-  const env = dotenv.config().parsed;
-  // reduce it to a nice object, the same as before
-  const envKeys = Object.keys(env).reduce((prev, next) => {
-    prev[`process.env.${next}`] = JSON.stringify(env[next]);
-    return prev;
-  }, {});
-  return {
-    entry: './src/index.js',
-    output: {
-      path: path.resolve(__dirname, 'dist'),
-      filename: 'index_bundle.js',
-      publicPath: '/'
-    },
-    module: {
-      rules: [
-        { test: /\.js$/, use: 'babel-loader' },
-        { test: /\.css$/, use: ['style-loader', 'css-loader'] }
-      ]
-    },
-    mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: './src/index.html',
-        favicon: './src/images/headerImg.ico'
-      }),
-      new webpack.DefinePlugin(envKeys)
-    ],
-    devServer: {
-      historyApiFallback: true
-    }
-  }
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index_bundle.js'
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, use: 'babel-loader' },
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] }
+    ]
+  },
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      favicon: './src/images/headerImg.ico'
+    })
+  ]
 }
